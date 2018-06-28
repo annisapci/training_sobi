411b35ca20a2b8725587c69add874e53ea100f7f
Syafira Arfianindya from Portcities <syafira@portcities.net>
2018-06-28 13:08:38 +0700

    [REM] remove unneessary function

--- training_invoice/models/account_invoice.py
+++ training_invoice/models/account_invoice.py
@@ -6,22 +6,6 @@ from odoo.addons import decimal_precision as dp
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    contact_id = fields.Many2one('res.partner', string='Contact')
-
-    @api.multi
-    @api.onchange('partner_id')
-    def partner_id_change(self):
-
-        res= {}
-        contact_list= []
-
-        for abc in self:
-            record = abc.partner_id.child_ids.filtered(lambda x: x.type == 'contact')
-            contact_list.append(record.ids)
-
-            res= {'domain':{'contact_id':[('id','in',contact_list[0])]}} 
-        return res
-
     discount_global = fields.Float(string="Discount Global")
     
     @api.one
